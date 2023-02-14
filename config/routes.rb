b890fcff848bc19735447794ef83f2fbd0c0707d
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-02-14 10:24:10 -0600

    Adds User controller, model, view, and ability to create a user and create a session.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Users
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   root to: 'people#index'
 
   resources :people do
