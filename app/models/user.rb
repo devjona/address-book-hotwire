e865834116cae9acc2dafa6a6bd60658e414cfe5
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-02-20 22:03:12 -0600

    Adds validations to user model, but they are still needed at database level. Updates nav as well to show user name when logged in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :people, dependent: :destroy
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: false
 end
