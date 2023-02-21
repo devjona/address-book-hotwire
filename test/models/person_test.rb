5324019df36c941b07efb4d286a6ed0a3cee6b5b
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-02-20 21:48:47 -0600

    Updates sign_up and person model tests. Passing.

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+  end
+
   test 'should not save a person without any info' do
     person = Person.new
     assert_not person.save
@@ -13,14 +17,12 @@ class PersonTest < ActiveSupport::TestCase
   end
 
   test 'should save a person with required info' do
-    person = Person.new
-    person.firstname = 'John'
-    person.lastname = 'Doe'
+    person = create_valid_person
     assert person.save
   end
 
   test 'should not save a person when dependent records are not properly saved' do
-    person = Person.new(firstname: 'John', lastname: 'Doe')
+    person = create_valid_person
     person.emails.build(address: '', comment: 'test')
     person.addresses.build(street: '', town: 'Anytown', zip: '12345')
     person.phones.build(number: '', comment: 'test')
@@ -29,11 +31,17 @@ class PersonTest < ActiveSupport::TestCase
   end
 
   test 'should save a person when dependent records are properly saved' do
-    person = Person.new(firstname: 'John', lastname: 'Doe')
+    person = create_valid_person
     person.emails.build(address: 'john@doe.com', comment: 'test')
     person.addresses.build(street: '123 Main St', town: 'Anytown', zip: '12345')
     person.phones.build(number: '1234567890', comment: 'test')
 
     assert person.save
+  end
+
+  private
+
+  def create_valid_person
+    Person.new(firstname: 'John', lastname: 'Doe', user_id: @user.id)
   end
 end
