e23cfbf3ee442a312cb7e18934aa3d10124c9e19
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-02-20 22:05:45 -0600

    Updates schema to enforce name, email, password_digest at DB level for User model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_022416) do
+ActiveRecord::Schema.define(version: 2023_02_21_040418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,9 +60,9 @@ ActiveRecord::Schema.define(version: 2023_02_21_022416) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
