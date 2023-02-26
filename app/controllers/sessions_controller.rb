31729086e75319f37fdb84eec61068a33d515b80
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-02-26 00:14:36 -0600

    Flash displsy message for invalid password/username combo.
    Session Controller tests needed still.
    
    We also need JSON tests for the User Controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to root_path
-      flash[:success] = "Welcome back, #{user.name}!"
+      flash[:success] = "Welcome back, #{@user.name}!"
     else
       redirect_to login_path
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash[:danger] = 'Invalid email/password combination'
     end
   end
 
